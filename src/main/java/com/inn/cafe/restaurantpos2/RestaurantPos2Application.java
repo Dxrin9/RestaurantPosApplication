c2e6f12a881e4fda4835599163506a2e42cba30c
Buhnă Dorin <buhna.dorin@gmail.com>
2026-02-24 17:20:25 +0200

    Start Project

---
+++ src/main/java/com/inn/cafe/restaurantpos2/RestaurantPos2Application.java
@@ -0,0 +1,13 @@
+package com.inn.cafe.restaurantpos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantPos2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantPos2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/inn/cafe/restaurantpos2/RestaurantPos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.cafe.restaurantpos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantPos2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
